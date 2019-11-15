39c009298f1abdeec0adf7c28377d580ad990a8e
jlaloi <julien.laloi@gmail.com>
2019-11-15 07:13:26 +0100

    From express to hapi

--- app/index.ts
+++ app/index.ts
@@ -1,10 +1,11 @@
-import * as express from 'express'
-import * as bodyParser from 'body-parser'
-import * as compression from 'compression'
 import * as fsWithCallbacks from 'fs'
+import * as Hapi from '@hapi/hapi'
+import * as Inert from '@hapi/inert'
+import * as Joi from '@hapi/joi'
 const fs = fsWithCallbacks.promises
+
 import {getLights, setBright, setPower, setRGB, initLookUpLight} from './light'
-import {getCasts, cast, castStop, initLookUpCast} from './cast'
+import {getCasts, cast, initLookUpCast} from './cast'
 
 const PORT = 9090
 const DIR_PUBLIC = 'public/'
@@ -16,62 +17,6 @@ const initLookup = () => {
   initLookUpCast()
 }
 
-const app = express()
-app.use(compression({filter: () => true}))
-app.use(bodyParser.json())
-
-app.get('/lookup', (req: express.Request, res: express.Response) => {
-  initLookup()
-  return res.json({
-    result: 'ok',
-  })
-})
-
-app.get('/light', (req: express.Request, res: express.Response) => res.json(getLights()))
-app.post('/light/:deviceId', async (req: express.Request, res: express.Response) => {
-  try {
-    const scene = req.body
-    if (scene.power) await setPower(req.params.deviceId, scene.power)
-    if (scene.rgb) await setRGB(req.params.deviceId, scene.rgb)
-    if (scene.bright) await setBright(req.params.deviceId, Number(scene.bright))
-    return res.json({result: 'ok'})
-  } catch (error) {
-    console.error(error)
-    return res.status(500).json({error: error.message})
-  }
-})
-
-app.get('/cast', (req: express.Request, res: express.Response) => res.json(getCasts()))
-app.post('/cast/:deviceId', async (req: express.Request, res: express.Response) => {
-  try {
-    return res.json(
-      await cast(
-        req.params.deviceId,
-        req.body.media,
-        DIR_PUBLIC + DIR_IMG,
-        (req.headers.referer || req.headers.host + '/') + DIR_IMG,
-      ),
-    )
-  } catch (error) {
-    console.error(error)
-    return res.status(500).json({error: error.message})
-  }
-})
-
-app.delete('/cast/:deviceId', async (req: express.Request, res: express.Response) => {
-  try {
-    return res.json(await castStop(req.params.deviceId))
-  } catch (error) {
-    console.error(error)
-    return res.status(500).json({error: error.message})
-  }
-})
-
-app.use(express.static(DIR_PUBLIC))
-app.get('/musics', async (req: express.Request, res: express.Response) =>
-  res.json(await readDir(DIR_PUBLIC + DIR_MUSIC, DIR_MUSIC)),
-)
-
 const readDir = async (path: string, prefix: string) => {
   const result = []
   for (const file of await fs.readdir(path)) {
@@ -82,9 +27,108 @@ const readDir = async (path: string, prefix: string) => {
   return result
 }
 
-app.get('/images', async (req: express.Request, res: express.Response) =>
-  res.json(await readDir(DIR_PUBLIC + DIR_IMG, DIR_IMG)),
-)
-app.listen(PORT, () => console.log(`Listening on port ${PORT}...`))
+const routes = [
+  {
+    method: 'GET',
+    path: '/{param*}',
+    handler: {
+      directory: {
+        path: DIR_PUBLIC,
+        index: ['index.html'],
+      },
+    },
+  },
+  {
+    method: 'GET',
+    path: '/images',
+    handler: async () => await readDir(DIR_PUBLIC + DIR_IMG, DIR_IMG),
+  },
+  {
+    method: 'GET',
+    path: '/musics',
+    handler: async () => await readDir(DIR_PUBLIC + DIR_MUSIC, DIR_MUSIC),
+  },
+  {
+    method: 'GET',
+    path: '/lookup',
+    handler: () => {
+      initLookup()
+      return 'ok'
+    },
+  },
+  {
+    method: 'GET',
+    path: '/light',
+    handler: getLights,
+  },
+  {
+    method: 'POST',
+    path: '/light/{deviceId}',
+    options: {
+      validate: {
+        params: Joi.object({
+          deviceId: Joi.string().required(),
+        }),
+        payload: Joi.object({
+          power: Joi.string().required(),
+          rgb: Joi.array().required(),
+          bright: Joi.number().required(),
+        }),
+      },
+    },
+    handler: async ({params: {deviceId}, payload: {power, rgb, bright}}: any) => {
+      if (power) await setPower(deviceId, power)
+      if (rgb) await setRGB(deviceId, rgb)
+      if (bright) await setBright(deviceId, Number(bright))
+      return {result: 'ok'}
+    },
+  },
+  {
+    method: 'GET',
+    path: '/cast',
+    handler: getCasts,
+  },
+  {
+    method: 'POST',
+    path: '/cast/{deviceId}',
+    options: {
+      validate: {
+        params: Joi.object({
+          deviceId: Joi.string().required(),
+        }),
+        payload: Joi.object({
+          media: Joi.string().required(),
+        }),
+      },
+    },
+    handler: async ({headers, params: {deviceId}, payload: {media}}: any) =>
+      await cast(deviceId, media, DIR_PUBLIC + DIR_IMG, (headers.referer || headers.host + '/') + DIR_IMG),
+  },
+]
 
+interface ReqCast extends Hapi.Request {
+  payload: {
+    media: string
+  }
+}
+
+interface ReqLight extends Hapi.Request {
+  payload: {
+    power: string
+    rgb: number[]
+    bright: number
+  }
+}
+
+const init = async () => {
+  const server = new Hapi.Server({
+    port: PORT,
+  })
+  await server.register(Inert)
+  server.route(routes)
+  await server.start()
+  console.log(`Server running on port ${PORT}`)
+}
+
+init()
 initLookup()
